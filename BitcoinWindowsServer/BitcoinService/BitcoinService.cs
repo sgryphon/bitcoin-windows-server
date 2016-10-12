ce2b01c7209e1732bdbbe1690c649bb4bd8322bd
Sly Gryphon <sgryphon@computer.org>
2016-10-12 12:48:05 +1000

    Simple service installer/runner, with logging

--- BitcoinWindowsServer/BitcoinService/BitcoinService.cs
+++ BitcoinWindowsServer/BitcoinService/BitcoinService.cs
@@ -12,17 +12,92 @@ namespace BitcoinService
 {
     public partial class BitcoinService : ServiceBase
     {
+        private TraceSource trace = new TraceSource("BitcoinService");
+
+        private Process bitcoindProcess;
+
+        private string bitcoindPath;
+
+        public string MainArgs
+        {
+            get;
+            set;
+        }
+
         public BitcoinService()
         {
+            trace.TraceEvent(TraceEventType.Information, 1001, "BitcoinService Initialize");
             InitializeComponent();
         }
 
         protected override void OnStart(string[] args)
         {
+            trace.TraceEvent(TraceEventType.Information, 1100, "BitcoinService Starting");
+            try
+            {
+                bitcoindPath = Environment.GetEnvironmentVariable("ProgramW6432");
+                bitcoindPath += "\\Bitcoin\\daemon\\bitcoind.exe";
+                trace.TraceEvent(TraceEventType.Verbose, 0, string.Format("Path: '{0}'", bitcoindPath));
+
+                bitcoindProcess = new Process();
+                bitcoindProcess.StartInfo = new ProcessStartInfo(bitcoindPath);
+                string startArgs = string.Join(" ", args);
+                trace.TraceEvent(TraceEventType.Verbose, 0, string.Format("StartArgs: '{0}'", startArgs));
+
+                if (!string.IsNullOrEmpty(startArgs))
+                {
+                    trace.TraceEvent(TraceEventType.Verbose, 0, "Using startArgs");
+                    bitcoindProcess.StartInfo.Arguments = startArgs;
+                }
+                else if (!string.IsNullOrEmpty(MainArgs))
+                {
+                    trace.TraceEvent(TraceEventType.Verbose, 0, "Using MainArgs");
+                    bitcoindProcess.StartInfo.Arguments = MainArgs;
+                }
+
+                bitcoindProcess.ErrorDataReceived += new DataReceivedEventHandler(Bitcoind_ErrorDataReceived);
+                bitcoindProcess.OutputDataReceived += new DataReceivedEventHandler(Bitcoind_OutputDataReceived);
+                bitcoindProcess.Exited += new EventHandler(Bitcoind_Exited);
+                bitcoindProcess.EnableRaisingEvents = true;
+
+                bool started = bitcoindProcess.Start();
+                trace.TraceEvent(TraceEventType.Verbose, 0, string.Format("Started: {0}", started));
+            }
+            catch (Exception ex)
+            {
+                trace.TraceEvent(TraceEventType.Error, 9100, string.Format("BitcoinService error starting: {0}", ex));
+            }
+        }
+
+        private void Bitcoind_Exited(object sender, EventArgs eventArgs)
+        {
+            int exitCode = bitcoindProcess.ExitCode;
+            trace.TraceEvent(TraceEventType.Verbose, 3, string.Format("EXITED: {0}", exitCode));
+        }
+
+        private void Bitcoind_OutputDataReceived(object sender, DataReceivedEventArgs eventArgs)
+        {
+            trace.TraceEvent(TraceEventType.Verbose, 1, string.Format("OUT: {0}", eventArgs.Data));
+        }
+
+        private void Bitcoind_ErrorDataReceived(object sender, DataReceivedEventArgs eventArgs)
+        {
+            trace.TraceEvent(TraceEventType.Warning, 2, string.Format("ERROR: {0}", eventArgs.Data));
         }
 
         protected override void OnStop()
         {
+            trace.TraceEvent(TraceEventType.Information, 8100, "BitcoinService Stopping");
+            try
+            {
+                bitcoindProcess.Kill();
+                bool exited = bitcoindProcess.WaitForExit(60000);
+                trace.TraceEvent(TraceEventType.Verbose, 0, string.Format("Bitcoin exit code: {0}", exited ? bitcoindProcess.ExitCode.ToString() : exited.ToString()));
+            }
+            catch (Exception arg)
+            {
+                trace.TraceEvent(TraceEventType.Error, 9101, string.Format("BitcoinService error stopping: {0}", arg));
+            }
         }
     }
 }
--- BitcoinWindowsServer/BitcoinService/Program.cs
+++ BitcoinWindowsServer/BitcoinService/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +11,70 @@ namespace BitcoinService
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main()
+        private static TraceSource trace = new TraceSource("BitcoinService");
+
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            bool hasArgs = args.Length != 0;
+            if (hasArgs)
             {
-                new BitcoinService()
+                var arg0 = args[0].ToLowerInvariant();
+
+                bool help = arg0.Equals("/help") || arg0.Equals("-help") || arg0.Equals("help")
+                    || arg0.Equals("/?") || arg0.Equals("-?") || arg0.Equals("?");
+                if (help)
+                {
+                    Console.WriteLine("Bitcoin Windows Service");
+                    Console.WriteLine("");
+                    Console.WriteLine(".\\BitcoinService.exe /install [\"service_args\"]");
+                    Console.WriteLine("  Installs the Windows Service 'bitcoind', to run with the given arguments");
+                    Console.WriteLine("  e.g. .\\BitcoinService.exe /install \"-datadir=F:\\Bitcoin\"");
+                    Console.WriteLine("");
+                    Console.WriteLine(".\\BitcoinService.exe /remove");
+                    Console.WriteLine("  Removes the Windows Service");
+                    return; 
+                }
+
+                bool install = arg0.Equals("/install") || arg0.Equals("-install") || arg0.Equals("install");
+                if (install)
+                {
+                    trace.TraceEvent(TraceEventType.Information, 1000, "BitcoinService Install");
+                    string mainArgsSuffix = string.Empty;
+                    bool hasMainArgs = args.Length > 1;
+                    if (hasMainArgs)
+                    {
+                        mainArgsSuffix = " " + args[1];
+                    }
+                    Process.Start("sc.exe", "create bitcoind binPath= \"" + Assembly.GetExecutingAssembly().Location + mainArgsSuffix + "\" start= auto obj= \"NT AUTHORITY\\Local Service\" password= \"\" DisplayName= \"Bitcoin Service\"").WaitForExit();
+                    Process.Start("sc.exe", "config bitcoind start= delayed-auto").WaitForExit();
+                    Console.WriteLine("Service Created");
+                    Console.WriteLine("Copy bitcoin.conf to the bitcoin datadir");
+                    Console.WriteLine("Default is C:\\Windows\\ServiceProfiles\\LocalService\\AppData\\Roaming\\Bitcoin");
+                    return;
+                }
+
+                bool remove = arg0.Equals("/remove") || arg0.Equals("-remove") || arg0.Equals("remove");
+                if (remove)
+                {
+                    trace.TraceEvent(TraceEventType.Information, 8000, "BitcoinService Remove");
+                    Process.Start("sc.exe", "stop bitcoind").WaitForExit();
+                    Process.Start("sc.exe", "delete bitcoind").WaitForExit();
+                    Console.WriteLine("Service Deleted");
+                    return;
+                }
+            }
+
+            trace.TraceEvent(TraceEventType.Information, 1002, "BitcoinService Main");
+            string mainArgs = string.Join(" ", args);
+            trace.TraceEvent(TraceEventType.Verbose, 0, string.Format("MainArgs: '{0}'", mainArgs));
+            var servicesToRun = new ServiceBase[]
+            {
+                new BitcoinService() {
+                    MainArgs = mainArgs
+                }
             };
-            ServiceBase.Run(ServicesToRun);
+            ServiceBase.Run(servicesToRun);
+            trace.TraceEvent(TraceEventType.Verbose, 0, "Exit Main");
         }
     }
 }
