103421efd26979fa131f16b66700a4febf15bad7
Sly Gryphon <sgryphon@computer.org>
2016-10-12 12:20:48 +1000

    Basic windows service project

---
+++ BitcoinWindowsServer/BitcoinService/BitcoinService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitcoinService
+{
+    public partial class BitcoinService : ServiceBase
+    {
+        public BitcoinService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
